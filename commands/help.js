839b6636d5cf73b013c99bb3f2a3dd3a3c4517e9
ASHUTOSH GOYAL <47284198+ashutoshgoy@users.noreply.github.com>
2021-06-03 15:05:03 +0530

    Add files via upload

---
+++ commands/help.js
@@ -0,0 +1,15 @@
+// Help fn 
+function helpFn() {
+    console.log(`
+    List of All the commands:
+                 node main.js tree "directoryPath"
+                node main.js organize "directoryPath"
+                node main.js help
+                `);
+}
+// method calling
+
+module.exports={
+    
+    helpKey: helpFn
+}
